4ace242d4b4162942fc13832f4d86a07c1caaeb1
henrykironde <henrykironde@gmail.com>
2019-03-05 00:11:45 -0500

    Create directory structure

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='idepend',
+    version='0.1.0',
+    description='idepend package for Python-Guide.org',
+    long_description=readme,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/henrykironde/idepend',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
